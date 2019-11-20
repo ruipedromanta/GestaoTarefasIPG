5e0f84318ec73e16fc6f902e18f4ef80e3fdbeee
HugoSantos29 <39484063+HugoSantos29@users.noreply.github.com>
2019-11-20 16:25:45 +0000

    criar modelo das funções

---
+++ GestaoTarefasIPG/Models/Funções.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestaoTarefasIPG.Models {
+    public class Funções {
+        
+        [Required]
+        public int FuncaoId { get; set; }
+
+       [Required]
+        public string NomeFuncao { get; set; }
+
+        [Required]
+        public string Attribute3 { get; set; }
+
+        [Required]
+        public string FuncionarioFuncionarioId { get; set; }
+
+
+
+    }
+}
