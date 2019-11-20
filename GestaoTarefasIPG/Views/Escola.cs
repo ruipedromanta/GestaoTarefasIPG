6c07f823d0fab52e054359b095829dc2e1494f38
pedroferreira96 <39617197+pedroferreira96@users.noreply.github.com>
2019-11-20 16:26:37 +0000

    Criado modelo Escola

---
+++ GestaoTarefasIPG/Views/Escola.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestaoTarefasIPG.Views {
+    public class Escola {
+        [Required]
+        public int EscolaId { get; set; }
+        [Required]
+        public string NomeEscola { get; set; }
+        [Required]
+        public string MoradaEscola { get; set; }
+        [Required]
+        [StringLength=9]
+        public string Telefone { get; set; }
+    }
+}
