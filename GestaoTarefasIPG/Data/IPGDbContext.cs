a8f2835b12f982c0a070a1ebdfa8bc8f0982cffc
pedroferreira96 <39617197+pedroferreira96@users.noreply.github.com>
2019-11-24 14:24:30 +0000

    Criado o IPGDbContext class na pasta Data

---
+++ GestaoTarefasIPG/Data/IPGDbContext.cs
@@ -0,0 +1,16 @@
+﻿using GestaoTarefasIPG.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestaoTarefasIPG.Data {
+    public class IPGDbContext : DbContext {
+        public IPGDbContext (DbContextOptions<IPGDbContext> options) : base(options) {
+
+        }
+
+        public DbSet<GestaoTarefasIPG.Models.Escola> Escola { get; set; }
+    }
+}
