b14ccfa77ed5520a2452aa26f8b1923b55ca5d27
HugoSantos29 <39484063+HugoSantos29@users.noreply.github.com>
2019-11-25 15:58:24 +0000

    criado o controlador para as funções (scaffolding)

---
+++ GestaoTarefasIPG/Data/IPGDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace GestaoTarefasIPG.Models
+{
+    public class IPGDbContext : DbContext
+    {
+        public IPGDbContext (DbContextOptions<IPGDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<GestaoTarefasIPG.Models.Funcao> Funcao { get; set; }
+    }
+}
