5e80fb58288a59e66294e67f2c4ca6749b5f5865
Rui Pedro Manta <ruipedromanta@hotmail.com>
2019-11-26 10:58:23 +0000

    Modelo do Departamento criado

---
+++ GestaoTarefasIPG/Models/Departamento.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestaoTarefasIPG.Models {
+    public class Departamento {
+
+        [Required]
+        public int DepartamentoId { get; set; }
+        [Required]
+        [StringLength(120)]
+        public string NomeDepartamento { get; set; }
+        [Required]
+        [StringLength(8)]
+        public string NumeroDepartamento { get; set; }
+    }
+}
