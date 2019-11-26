00dab3c22b7cc8733972ba0ae89950a8bc0074b4
Rui Pedro Manta <ruipedromanta@hotmail.com>
2019-11-26 11:01:52 +0000

    Criado o Controlador Departamento

--- GestaoTarefasIPG/Startup.cs
+++ GestaoTarefasIPG/Startup.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -12,6 +12,7 @@ using GestaoTarefasIPG.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using GestaoTarefasIPG.Models;
 
 namespace GestaoTarefasIPG
 {
@@ -34,6 +35,9 @@ namespace GestaoTarefasIPG
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
             services.AddRazorPages();
+
+            services.AddDbContext<IPGDbContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("IPGDbContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
