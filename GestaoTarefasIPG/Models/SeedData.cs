2a8f4719d94a3cfaef9c2f5e8e6b143aac69dfa7
HugoSantos29 <39484063+HugoSantos29@users.noreply.github.com>
2019-12-03 11:37:58 +0000

    Criado o SeedData

---
+++ GestaoTarefasIPG/Models/SeedData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestaoTarefasIPG.Models {
+    public class SeedData {
+
+        public static void Populate(IPGDbContext db) {
+            if (db.Funcao.Any()) {
+                return;
+            }
+
+
+            db.Funcao.AddRange(
+                new Funcao { NomeFuncao = "Secretaria" },
+                new Funcao { NomeFuncao = "Tesouraria" },
+                new Funcao { NomeFuncao = "Reprografia" },
+                new Funcao { NomeFuncao = "Serviços Sociais" },
+                new Funcao { NomeFuncao = "Cantina" },
+                new Funcao { NomeFuncao = "Papelaria" }
+
+
+
+                );
+            db.SaveChanges();
+        }
+    }
+}
