38efe2a79973a49f63bcaddf63ff25d36f65807f
Rui Pedro Manta <ruipedromanta@hotmail.com>
2019-12-03 15:37:17 +0000

    1

--- GestaoTarefasIPG/Models/SeedData.cs
+++ GestaoTarefasIPG/Models/SeedData.cs
@@ -6,24 +6,39 @@ using System.Threading.Tasks;
 namespace GestaoTarefasIPG.Models {
     public class SeedData {
 
+        private static void AdicionaFuncoes(IPGDbContext db) {
+            if (db.Funcao.Any()) {
+                return;
+            }
+
+            db.Funcao.AddRange(
+                new Funcao { NomeFuncao = "Secretaria" },
+                new Funcao { NomeFuncao = "Tesouraria" },
+                new Funcao { NomeFuncao = "Reprografia" },
+                new Funcao { NomeFuncao = "Serviços Sociais" },
+                new Funcao { NomeFuncao = "Cantina" },
+                new Funcao { NomeFuncao = "Papelaria" }
+                );
+
+            db.SaveChanges();
+        }
         public static void AdicionaDepartamentos(IPGDbContext db) {
             if (db.Departamento.Any()) {
                 return;
             }
-
-
             db.Departamento.AddRange(
-                new Departamento { NomeDepartamento = "Secretaria"},
-                new Departamento { NomeDepartamento = "Tesouraria"},
-                new Departamento { NomeDepartamento = "Reprografia"},
-                new Departamento { NomeDepartamento = "Serviços Sociais"},
-                new Departamento { NomeDepartamento = "Cantina"},
-                new Departamento { NomeDepartamento = "Papelaria"}
-                
-
-
+                new Departamento { NomeDepartamento = "Secretaria" },
+                new Departamento { NomeDepartamento = "Tesouraria" },
+                new Departamento { NomeDepartamento = "Reprografia" },
+                new Departamento { NomeDepartamento = "Serviços Sociais" },
+                new Departamento { NomeDepartamento = "Cantina" },
+                new Departamento { NomeDepartamento = "Papelaria" }
                 );
             db.SaveChanges();
-                }
+        }
+
     }
 }
+
+
+        
