033a99ee189a50d7ee2b3eac62e363157d51c040
Rui Pedro Manta <ruipedromanta@hotmail.com>
2019-12-03 12:22:35 +0000

    Merge SeedData

--- GestaoTarefasIPG/Models/SeedData.cs
+++ GestaoTarefasIPG/Models/SeedData.cs
@@ -6,24 +6,38 @@ using System.Threading.Tasks;
 namespace GestaoTarefasIPG.Models {
     public class SeedData {
 
-        public static void Populate(DepartamentoDbContext db) {
-            if (db.Departamento.Any()) {
+        private static void AdicionaFuncoes(IPGDbContext db) {
+            if (db.Funcao.Any()) {
                 return;
             }
 
-
-            db.Departamento.AddRange(
-                new Departamento { NomeDepartamento = "Secretaria", NumeroDepartamento = "15874698"},
-                new Departamento { NomeDepartamento = "Tesouraria", NumeroDepartamento = "15324896"},
-                new Departamento { NomeDepartamento = "Reprografia", NumeroDepartamento = "15247896"},
-                new Departamento { NomeDepartamento = "Serviços Sociais", NumeroDepartamento = "15247896"},
-                new Departamento { NomeDepartamento = "Cantina", NumeroDepartamento = "15478563"},
-                new Departamento { NomeDepartamento = "Papelaria", NumeroDepartamento = "15478965"}
-                
-
-
+            db.Funcao.AddRange(
+                new Funcao { NomeFuncao = "Secretaria" },
+                new Funcao { NomeFuncao = "Tesouraria" },
+                new Funcao { NomeFuncao = "Reprografia" },
+                new Funcao { NomeFuncao = "Serviços Sociais" },
+                new Funcao { NomeFuncao = "Cantina" },
+                new Funcao { NomeFuncao = "Papelaria" }
                 );
+
             db.SaveChanges();
-                }
+        }
+
+
+
+        private static void AdicionaDepartamentos(IPGDbContext db) {
+            if (db.Departamento.Any()) {
+                return;
+            }
+            db.Departamento.AddRange(
+                new Departamento { NomeDepartamento = "Secretaria" },
+                new Departamento { NomeDepartamento = "Tesouraria" },
+                new Departamento { NomeDepartamento = "Reprografia" },
+                new Departamento { NomeDepartamento = "Serviços Sociais" },
+                new Departamento { NomeDepartamento = "Cantina" },
+                new Departamento { NomeDepartamento = "Papelaria" }
+                );
+
+        }
     }
 }
