7e07797cb384d7efbb02c25664cb871b5b69fde8
Rui Pedro Manta <ruipedromanta@hotmail.com>
2019-12-04 15:23:26 +0000

    Alterações no Seed Data

--- GestaoTarefasIPG/Models/SeedData.cs
+++ GestaoTarefasIPG/Models/SeedData.cs
@@ -6,18 +6,23 @@ using System.Threading.Tasks;
 namespace GestaoTarefasIPG.Models {
     public class SeedData {
 
-        public static void Populate(IPGDbContext db) {
-            if (db.Departamento.Any()) {
+        public static void AdicionaFuncoes(IPGDbContext db) {
+            if (db.Funcao.Any()) {
                 return;
             }
 
             db.Funcao.AddRange(
-                new Funcao { NomeFuncao = "" },
-                new Funcao { NomeFuncao = "" },
-                new Funcao { NomeFuncao = "" },
-                new Funcao { NomeFuncao = " " },
-                new Funcao { NomeFuncao = "" },
-                new Funcao { NomeFuncao = "" }
+                new Funcao { NomeFuncao = "Assistente Operacional" },
+                new Funcao { NomeFuncao = "Professor" },
+                new Funcao { NomeFuncao = "Assistente Técnica" },
+                new Funcao { NomeFuncao = "Bibliotecária" },
+                new Funcao { NomeFuncao = "Diretor" },
+                new Funcao { NomeFuncao = "Presidente" },
+                new Funcao { NomeFuncao = "Tesoureiro" },
+                new Funcao { NomeFuncao = "Vice-Presidente" },
+                new Funcao { NomeFuncao = "Secretária" }
+                
+
                 );
             db.SaveChanges();
         }
@@ -32,6 +37,20 @@ namespace GestaoTarefasIPG.Models {
                 new Departamento { NomeDepartamento = "Serviços Sociais" },
                 new Departamento { NomeDepartamento = "Cantina" },
                 new Departamento { NomeDepartamento = "Papelaria" }
+                );
+            db.SaveChanges();
+        }
+
+        public static void AdicionaEscolas(IPGDbContext db) {
+            if (db.Escola.Any()) {
+                return;
+            }
+            db.Escola.AddRange(
+                new Escola {NomeEscola = "Escola Superior de Saúde", Telefone = "271205220" },
+                new Escola { NomeEscola = "Escola Superior de Educação, Comunicação e Desporto", Telefone = "271220135" },
+                new Escola {NomeEscola = "Escola Superior de Tecnologia e Gestão", Telefone = "271220164" },
+                new Escola {NomeEscola = "Escola Superior de Turismo e Hotelaria", Telefone = "238320800" }
+                
                 );
             db.SaveChanges();
         }
--- GestaoTarefasIPG/Startup.cs
+++ GestaoTarefasIPG/Startup.cs
@@ -14,20 +14,16 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using GestaoTarefasIPG.Models;
 
-namespace GestaoTarefasIPG
-{
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
+namespace GestaoTarefasIPG {
+    public class Startup {
+        public Startup(IConfiguration configuration) {
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
+        public void ConfigureServices(IServiceCollection services) {
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
@@ -41,39 +37,47 @@ namespace GestaoTarefasIPG
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
+            if (env.IsDevelopment()) {
                 using (var ServiceScope = app.ApplicationServices.CreateScope()) {
                     var db = ServiceScope.ServiceProvider.GetService<IPGDbContext>();
                     SeedData.AdicionaDepartamentos(db);
                 }
-                
-                app.UseDeveloperExceptionPage();
-                app.UseDatabaseErrorPage();
+
+                if (env.IsDevelopment()) {
+                    using (var ServiceScope = app.ApplicationServices.CreateScope()) {
+                        var db = ServiceScope.ServiceProvider.GetService<IPGDbContext>();
+                        SeedData.AdicionaEscolas(db);
+                    }
+
+                    if (env.IsDevelopment()) {
+                        using (var ServiceScope = app.ApplicationServices.CreateScope()) {
+                            var db = ServiceScope.ServiceProvider.GetService<IPGDbContext>();
+                            SeedData.AdicionaFuncoes(db);
+                        }
+                        app.UseDeveloperExceptionPage();
+                        app.UseDatabaseErrorPage();
+                    } else {
+                        app.UseExceptionHandler("/Home/Error");
+                        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                        app.UseHsts();
+                    }
+                    app.UseHttpsRedirection();
+                    app.UseStaticFiles();
+
+                    app.UseRouting();
+
+                    app.UseAuthentication();
+                    app.UseAuthorization();
+
+                    app.UseEndpoints(endpoints => {
+                        endpoints.MapControllerRoute(
+                            name: "default",
+                            pattern: "{controller=Home}/{action=Index}/{id?}");
+                        endpoints.MapRazorPages();
+                    });
+                }
             }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-
-            app.UseRouting();
-
-            app.UseAuthentication();
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
-                endpoints.MapRazorPages();
-            });
         }
     }
 }
