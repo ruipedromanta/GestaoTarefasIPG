2ca750de120c5a9e171422f96c09558e9fb5201d
HugoSantos29 <39484063+HugoSantos29@users.noreply.github.com>
2019-12-09 20:05:38 +0000

    Criado o FuncoesViewModel

---
+++ GestaoTarefasIPG/Models/FuncoesViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestaoTarefasIPG.Models {
+    public class FuncoesViewModel {
+        public IEnumerable<Funcao> Funcoes { get; set; }
+
+        public int PaginaAtual { get; set; }
+
+        public int TotalPaginas { get; set; }
+
+        public int PrimeiraPagina { get; set; }
+
+        public int UltimaPagina { get; set; }
+    }
+} 
+    
+
