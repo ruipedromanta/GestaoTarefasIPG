41d07793a01afa438190e9b1ba6675f49b76d56a
pedroferreira96 <39617197+pedroferreira96@users.noreply.github.com>
2019-12-09 16:52:40 +0000

    Criado o EscolasViewModel

---
+++ GestaoTarefasIPG/Models/EscolasViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestaoTarefasIPG.Models {
+    public class EscolasViewModel {
+        public IEnumerable<Escola> Escolas { get; set; }
+        public int CurrentPage { get; set; }
+        public int TotalPages { get; set; }
+        public int FirstPageShow { get; set; }
+        public int LastPageShow { get; set; }
+    }
+}
