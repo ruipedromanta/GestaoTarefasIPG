406d7ab02c0b07f15c80a51edc36fe74f37e32fc
pedroferreira96 <39617197+pedroferreira96@users.noreply.github.com>
2019-12-09 16:58:19 +0000

    Criado o modelo IGestaoTarefasIPGRepository

---
+++ GestaoTarefasIPG/Models/IGestaoTarefasIPGRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestaoTarefasIPG.Models {
+    public interface IGestaoTarefasIPGRepository {
+        public IEnumerable<Escola> Escolas { get; }
+    }
+}
