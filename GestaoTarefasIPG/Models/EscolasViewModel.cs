00f9b88e0ecf2f032063ad63ff2ccffaad2579ba
pedroferreira96 <39617197+pedroferreira96@users.noreply.github.com>
2020-01-15 15:12:03 +0000

    Procura e ordenação concluída

--- GestaoTarefasIPG/Models/EscolasViewModel.cs
+++ GestaoTarefasIPG/Models/EscolasViewModel.cs
@@ -10,9 +10,7 @@ namespace GestaoTarefasIPG.Models {
         public int TotalPages { get; set; }
         public int FirstPageShow { get; set; }
         public int LastPageShow { get; set; }
-
         public string StringProcura { get; set; }
-
         public string Sort { get; set; }
     }
 }
