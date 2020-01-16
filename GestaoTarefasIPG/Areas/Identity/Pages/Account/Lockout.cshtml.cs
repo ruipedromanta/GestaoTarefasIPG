5db6b3690f4dcefb62cee85d7f86f9a0854178f5
Rui Pedro Manta <ruipedromanta@hotmail.com>
2020-01-16 22:33:39 +0000

    Login e Logout

---
+++ GestaoTarefasIPG/Areas/Identity/Pages/Account/Lockout.cshtml.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GestaoTarefasIPG.Areas.Identity.Pages.Account {
+    [AllowAnonymous]
+    public class LockoutModel : PageModel {
+        public void OnGet() {
+
+        }
+    }
+}
