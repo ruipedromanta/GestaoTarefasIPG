478911a819397271af16102ee329e09af2d7aad5
Rui Pedro Manta <ruipedromanta@hotmail.com>
2020-01-20 20:25:38 +0000

    Criação do Role management

--- GestaoTarefasIPG/Models/SeedData.cs
+++ GestaoTarefasIPG/Models/SeedData.cs
@@ -7,9 +7,8 @@ using System.Threading.Tasks;
 namespace GestaoTarefasIPG.Models {
     public class SeedData {
 
-
         public const string ADMIN_ROLE = "admin";
-
+        public const string MANAGEMENT_ROLE = "manager";
 
         public static void AdicionaDepartamentos(IPGDbContext db) {
             if (db.Departamento.Any()) {
@@ -22,30 +21,20 @@ namespace GestaoTarefasIPG.Models {
                 new Departamento { NomeDepartamento = "Serviços Sociais" },
                 new Departamento { NomeDepartamento = "Cantina" },
                 new Departamento { NomeDepartamento = "Papelaria" }
-
-
-
-
                 );
             db.SaveChanges();
-
-
         }
-
-
         public static void AdicionaEscolas(IPGDbContext db) {
 
             if (db.Escola.Any()) {
                 return;
             }
-
             db.Escola.AddRange(
                 new Escola { NomeEscola = "Escola Superior de Saúde", Telefone = "271205220"},
                 new Escola { NomeEscola = "Escola Superior de Educação, Comunicação e Desporto", Telefone = "271220135" },
                 new Escola { NomeEscola = "Escola Superior de Tecnologia e Gestão", Telefone = "271220164" },
                 new Escola { NomeEscola = "Escola Superior de Turismo e Hotelaria", Telefone = "238320800" }
                 );
-
             db.SaveChanges();
         }
 
@@ -53,18 +42,12 @@ namespace GestaoTarefasIPG.Models {
             if (db.Funcao.Any()) {
                 return;
             }
-
-
             db.Funcao.AddRange(
                 new Funcao { NomeFuncao = "Presidente" },
                 new Funcao { NomeFuncao = "Vice Presidente" },
                 new Funcao { NomeFuncao = "Diretor" },
                 new Funcao { NomeFuncao = "Auxiliar" },
                 new Funcao { NomeFuncao = "Professor" }
-
-
-
-
                 );
             db.SaveChanges();
         }
@@ -73,6 +56,9 @@ namespace GestaoTarefasIPG.Models {
         public static async Task PopulateUsersAsync(UserManager<IdentityUser> userManager) {
             const string ADMIN_USERNAME = "admin@ipg.pt";
             const string ADMIN_PASSWORD = "Secret123$";
+
+            const string MANAGEMENT_ROLE = "management@ipg.pt";
+            const string MANAGEMENT_PASSWORD = "Manager123@";
 
             IdentityUser user = await userManager.FindByNameAsync(ADMIN_USERNAME);
             if (user == null) {
@@ -83,6 +69,8 @@ namespace GestaoTarefasIPG.Models {
 
                 await userManager.CreateAsync(user, ADMIN_PASSWORD);
             }
+
+
             if (!await userManager.IsInRoleAsync(user, ADMIN_ROLE)) {
                 await userManager.AddToRoleAsync(user, ADMIN_ROLE);
             }
